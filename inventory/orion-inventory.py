ab4865229a17023e167dbd332c29efc8aa9095d2
Andy Escolastico <aesubs@icloud.com>
2021-02-02 16:30:01 -0500

    added credential validation logic

--- inventory/orion-inventory.py
+++ inventory/orion-inventory.py
@@ -8,6 +8,13 @@ import json
 import argparse
 import re
 import warnings
+import sys
+
+try:
+    from creds import orion_creds
+except:
+    pass
+
 
 class OrionInventory(object):
     def __init__(self):
@@ -23,9 +30,9 @@ class OrionInventory(object):
         parser = argparse.ArgumentParser()
         parser.add_argument('--list', action = 'store_true')
         parser.add_argument('--host', action = 'store')
-        parser.add_argument('--server', action='store', type=str, required=True)
-        parser.add_argument('--username', action='store', type=str, required=True)
-        parser.add_argument('--password', action='store', type=str, required=True)
+        parser.add_argument('--server', action = 'store')
+        parser.add_argument('--username', action = 'store')
+        parser.add_argument('--password', action = 'store')
         self.args = parser.parse_args()
     def empty_inventory(self):
         return {'_meta': {'hostvars': {}}}        
@@ -33,10 +40,25 @@ class OrionInventory(object):
     def normalize_inventory(item):
         item = (re.sub('[^A-Za-z0-9]+', '_', item)).upper()
         return item
+    def validate_credentials(self):
+        if self.args.server and self.args.username and self.args.password:
+            self.server = self.args.server
+            self.username = self.args.username
+            self.password = self.args.password
+        elif "creds.orion_creds" in sys.modules:
+            if orion_creds.server and orion_creds.username and orion_creds.password:
+                self.server = orion_creds.server
+                self.username = orion_creds.username
+                self.password = orion_creds.password
+            else:
+                sys.exit("[ERROR] No creds found. Please provide credentials either by passing the script arguments or by populating a 'creds/orion_creds.py' file")
+        else:
+            sys.exit("[ERROR] No creds found. Please provide credentials either by passing the script arguments or by populating a 'creds/orion_creds.py' file")
     def collect_inventory(self):
-        username = self.args.username
-        password = self.args.password
-        server = self.args.server
+        self.validate_credentials()
+        server = self.server
+        username = self.username
+        password = self.password
         url = "https://" + server + ":17778/SolarWinds/InformationService/v3/Json/Query"
         query = "SELECT n.IPAddress, p.CWID, p.CustomerName FROM Orion.Nodes AS n JOIN Orion.NodesCustomProperties AS p on n.NodeID = p.NodeID WHERE n.Status!= 9"
         params = "query=" + query
